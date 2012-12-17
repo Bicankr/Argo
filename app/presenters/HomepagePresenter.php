e501989787588d18a87897d81ea6053b66f810ef
zednikp <zednikp@VM-7.tescosw.loc>
2012-12-17 17:01:04 +0100

    Před push remote

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,11 +5,21 @@
  */
 class HomepagePresenter extends BasePresenter {
 
+    private $menu;
+
     public function renderDefault() {
-	$this->template->anyVariable = 'any value';
+        $this->template->anyVariable = 'any value';
     }
+    public function rendervyzivarostlin($p_menu) {
+	$this->template->menu = $p_menu;
+    }
+    public function renderochranarostlin($p_menu) {
+	$this->template->menu = $p_menu;
+    }
+
     public function show() {
-	$this->template->anyVariable = 'show';
+        $this->template->anyVariable = 'show';
     }
+
 
 }
